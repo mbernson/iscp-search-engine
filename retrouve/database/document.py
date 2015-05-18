7937e3fa8460e58e0455388463a87168d3db1a40
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-05-18 21:05:42 +0200

    Stuff

---
+++ retrouve/database/document.py
@@ -0,0 +1,10 @@
+from retrouve.database.model import Model
+from retrouve.database.url import Url
+
+class Document(Model):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def from_url(url):
+        pass
