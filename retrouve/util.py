3485d853fc7880b80c33513a0d4d98201f9316cd
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-05-31 21:24:38 +0200

    Docs

--- retrouve/util.py
+++ retrouve/util.py
@@ -1,4 +1,15 @@
+"""
+Utility functions
+"""
+
 def merge_dicts(a, b):
+    """
+    Merge two dictionaries, overwriting the keys from dict `a` with those from
+    dict `b` should they conflict.
+    :param a: dict
+    :param b: dict
+    :return: dict
+    """
     temp = a.copy()
     temp.update(b)
     return temp
